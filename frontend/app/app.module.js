6f331d4ddedb577086806fd7d5347e909df284fa
Nicolò <nicolo.frison97@gmail.com>
2020-11-12 14:00:35 +0100

    Added navbar module to app module

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -1,3 +1,5 @@
-angular.module('phonecatApp', [
-    'ngRoute'
-]);
+angular
+    .module('app', [
+        'myNavbar',
+        'ngRoute'
+    ]);
---
+++ frontend/app/components/navbar/navbar.component.js
@@ -0,0 +1,8 @@
+angular
+    .module('myNavbar')
+    .component('myNavbar', {
+        templateUrl: '/app/components/navbar/navbar.html',
+        controller: function () {
+            console.log("mynavbar");
+        }
+    });

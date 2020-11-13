2da557714679cbdada6e011a000aa336b9d26972
Nicolò <nicolo.frison97@gmail.com>
2020-11-13 13:20:10 +0100

    Added eslint

--- frontend/src/app.module.js
+++ frontend/src/app.module.js
@@ -1,3 +1,27 @@
-angular.module('app', [
-    'ngRoute'
-]);
+import angular from 'angular';
+// eslint-disable-next-line no-unused-vars
+import ngRoute from 'angular-route';
+import { videosService } from './services/episodes';
+
+const apiBaseUrl = 'http://localhost:3100';
+
+angular
+  .module('app', [
+    'ngRoute',
+  ])
+  .config(['$routeProvider',
+    function config($routeProvider) {
+      $routeProvider
+        .when('/phones', {
+          template: 'phones',
+        })
+        .when('/phones/:phoneId', {
+          template: 'phone',
+        })
+        .otherwise('/phones');
+    },
+  ])
+  .controller('appCtrl', [async function () {
+    videosService.setBaseUrl(apiBaseUrl);
+    console.log(await videosService.getVideos());
+  }]);

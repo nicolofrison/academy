f4df5aa4e83d6041f546c72aa179ac1a52c538e7
Nicolò <nicolo.frison97@gmail.com>
2020-11-13 15:30:08 +0100

    Converted navbar component html import to es6 import

---
+++ frontend/src/app.config.js
@@ -0,0 +1,55 @@
+import appModule from './app.module';
+
+//  services
+import { videosService } from './services/episodes';
+
+//  components
+import './components/navbar/navbar.component';
+
+//  variables
+const apiBaseUrl = 'http://localhost:3100';
+
+appModule
+  .config(['$routeProvider', function config($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        template: '<my-login></my-login>',
+      })
+      .when('/home', {
+        template: '<my-home></my-home>',
+      })
+      .when('/search', {
+        template: '<my-search></my-search>',
+      })
+      .when('/movies', {
+        template: '<my-movies></my-movies>',
+      })
+      .when('/details', {
+        template: '<my-details></my-details>',
+      })
+      .when('/signUp', {
+        template: '<my-sign-up></my-sign-up>',
+      })
+      .when('/profile', {
+        template: '<my-profile></my-profile>',
+      })
+      .when('/filmdetails', {
+        template: '<my-film-details></my-film-details>',
+      })
+      .otherwise('/home');
+  }])
+  .factory('storageService', () => ({
+    get(key) {
+      console.log('get');
+      return sessionStorage.getItem(key);
+    },
+    save(key, data) {
+      console.log('set');
+      sessionStorage.setItem(key, data);
+    },
+  }))
+  .controller('appCtrl', ['storageService', '$scope', async (storageService, $scope) => {
+    $scope.isLogged = () => storageService.get('loggedIn') === 'true';
+    videosService.setBaseUrl(apiBaseUrl);
+    console.log(await videosService.getVideos());
+  }]);

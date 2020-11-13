16475121fac6e1f6fe81243a1a0021a41aa799f6
Davide <davide.carraro1998@gmail.com>
2020-11-13 11:25:44 +0100

    Added ng-repeat in rows

--- frontend/app/components/row/row.component.js
+++ frontend/app/components/row/row.component.js
@@ -1,4 +1,21 @@
 angular.module('app').component('myRow', 
 {
-    templateUrl: 'app/components/row/row.html'
+    templateUrl: 'app/components/row/row.html',
+    controller: function() {
+        const listFilm = [{
+            title: 'title1'
+        },{
+            title: 'title2'
+        },{
+            title: 'title3'
+        },{
+            title: 'title4'
+        },{
+            title: 'title5'
+        }];
+
+        this.listFilm  = listFilm; 
+    }
+
+
 });

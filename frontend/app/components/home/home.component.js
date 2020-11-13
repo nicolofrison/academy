9e538a4393371d4d3ac27b8758b19c15be7624f9
Davide <davide.carraro1998@gmail.com>
2020-11-13 15:52:24 +0100

    Fix bindings into rows with home links

--- frontend/app/components/home/home.component.js
+++ frontend/app/components/home/home.component.js
@@ -1,4 +1,12 @@
+function homeController() {
+    var ctrl = this;
+    ctrl.movies = 'movies';
+    ctrl.series = 'series';
+}
+
 angular.module('app').component('myHome', 
 { 
-    templateUrl: 'app/components/home/home.html'
+    templateUrl: 'app/components/home/home.html',
+    controllerAs: 'homeCtrl',
+    controller: homeController
 });

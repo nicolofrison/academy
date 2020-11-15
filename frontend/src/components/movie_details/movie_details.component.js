785f2bc051647c80e05d8fdad78a3d6615be4ef9
Nicolò <nicolo.frison97@gmail.com>
2020-11-15 18:48:06 +0100

    Fix css injector for components

--- frontend/src/components/movie_details/movie_details.component.js
+++ frontend/src/components/movie_details/movie_details.component.js
@@ -1,9 +1,16 @@
 import appModule from '../../app.module';
 
+const cssPath = '/src/components/movie_details/movie_details.css';
+
 appModule
   .component('myMovieDetails', {
     templateUrl: '/src/components/movie_details/movie_details.html',
-    controller($scope, cssInjector) {
-      cssInjector.add('/src/components/movie_details/movie_details.css');
-    },
+    controller: ['$scope', 'cssInjector', function ($scope, cssInjector) {
+      this.$onInit = function () {
+        cssInjector.add(cssPath);
+      };
+      this.$onDestroy = function () {
+        cssInjector.remove(cssPath);
+      };
+    }],
   });
--- frontend/src/components/profile/profile.component.js
+++ frontend/src/components/profile/profile.component.js
@@ -1,9 +1,16 @@
 import appModule from '../../app.module';
 
+const cssPath = '/src/components/profile/changeProfile.css';
+
 appModule
   .component('myProfile', {
     templateUrl: '/src/components/profile/profile.html',
-    controller: ['cssInjector', (cssInjector) => {
-      cssInjector.add('/src/components/profile/changeProfile.css');
+    controller: ['cssInjector', function (cssInjector) {
+      this.$onInit = function () {
+        cssInjector.add(cssPath);
+      };
+      this.$onDestroy = function () {
+        cssInjector.remove(cssPath);
+      };
     }],
   });
